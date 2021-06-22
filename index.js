a4b1adf91307ccd4b30e206d6b4a3bc2c904c31e
Ruben Cordero <rubencordero01@gmail.com>
2021-06-22 18:04:07 -0500

    Day two updates to all files

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-const player1 = window.prompt("Enter name for player 1: ");
-const player2 = window.prompt("Enter name for player 2: ");
+// const player1 = window.prompt("Enter name for player 1: ");
+// const player2 = window.prompt("Enter name for player 2: ");
 
-let currentPlayer = [1];
-document.querySelector(".players-names").innerHTML = `${player1} vs ${player2}`;
+// document.querySelector(".players-names").innerHTML = `${player1} vs ${player2}`;
 
-// if(currentPlayer = 1){
-// document.querySelector(".players-turn").innerHTML = ` It's ${player1}'s turn! `
-// } else document.querySelector(".players-turn").innerHTML = ` It's ${player2}'s turn! `
+// function createDivs(){
+// for ( i = 0; i< 42; i++){
+//    let newDiv = document.createElement("div")
+//    divEl.appendChild(newDiv);  
+// }
+// }
+// createDivs();
 
-document.addEventListener("click", () => {
-  const divEl = document.querySelectorAll(".grid-container div");
+let currentPlayer = 1;
 
+const divEl = document.querySelectorAll(".grid-container div");
+
+const divArray = [];
+for (let i = 0; i < divEl.length; i++){
+    divArray.push(divEl[i])
+}
+
+let render = () => {
   for (let i = 0; i < divEl.length; i++) {
     divEl[i].onclick = () => {
-      if (currentPlayer == 1) {
-        red = divEl[i].style.backgroundColor = "red";
-        divEl[i].classList.add("bottom-row");
+      if (currentPlayer == 1 && divEl[i].classList.contains("open")) {
+        divEl[i].style.backgroundColor = "red";
+        divEl[i].classList.add("player-1");
+        divEl[i].classList.remove("open");
         currentPlayer = 2;
-        document.querySelector(
-          ".players-turn"
-        ).innerHTML = ` It's ${player2}'s turn! `;
-      } else if (currentPlayer == 2) {
-        blue =  divEl[i].style.backgroundColor = "blue";
-        divEl[i].classList.add("bottom-row");
+      } else if (currentPlayer == 2 && divEl[i].classList.contains("open")) {
+        divEl[i].style.backgroundColor = "blue";
+        divEl[i].classList.add("player-2");
+        divEl[i].classList.remove("open");
         currentPlayer = 1;
-        document.querySelector(
-          ".players-turn"
-        ).innerHTML = ` It's ${player1}'s turn! `;
+
+        // document.querySelector(
+        //   ".players-turn"
+        // ).innerHTML = ` It's ${player2}'s turn! `;
+        } else (alert("move not allowed"))
+        // document.querySelector(
+        //   ".players-turn"
+        // ).innerHTML = ` It's ${player1}'s turn! `;
+        console.log(divEl)
       }
-    };
-  }
-});
+    }
+}
+
+
+
+// function check (){
+//     if (divArray[0].classList.contains("player-1") &&
+//         divArray[1].classList.contains("player-1") &&
+//         divArray[2].classList.contains("player-1") &&
+//         divArray[3].classList.contains("player-1")){
+//             alert("player one wins")
+// } 
+// }
+
+render();
