fefadcf10f5289b537e4438832f5bea49e4d07eb
Ruben Cordero <rubencordero01@gmail.com>
2021-06-23 21:53:42 -0500

    Finally a mf MVP

--- index.js
+++ index.js
@@ -1,70 +1,287 @@
-// const player1 = window.prompt("Enter name for player 1: ");
-// const player2 = window.prompt("Enter name for player 2: ");
+// Cached Elements
+const gameBoard = document.querySelector('.grid-container')
+const tiles = document.querySelectorAll('.grid-container div');
 
-// document.querySelector(".players-names").innerHTML = `${player1} vs ${player2}`;
+let currentPlayer = 1
 
-// function createDivs(){
-// for ( i = 0; i< 42; i++){
-//    let newDiv = document.createElement("div")
-//    divEl.appendChild(newDiv);
-// }
-// }
-// createDivs();
+let board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-// const divArray = [];
-// for (let i = 0; i < divEl.length; i++){
-//     divArray.push(divEl[i])
-// }
+gameBoard.addEventListener('click', render)
 
-let currentPlayer = 1;
-
-const divEl = document.querySelectorAll(".grid-container div");
-
-function check() {
-  for (let i = 0; i < divEl.length; i++)
-    //Check for a horizontal win for player one
-    if (
-      divEl[i].classList.contains("player-1") &&
-      divEl[i + 1].classList.contains("player-1") &&
-      divEl[i + 2].classList.contains("player-1") &&
-      divEl[i + 3].classList.contains("player-1") &&
-      divEl[i] != [5, 11, 17, 23, 29, 35, 41]
-    ) {
-      alert("Player one wins!");
-    } else if (
-      divEl[i].classList.contains("player-2") &&
-      divEl[i + 1].classList.contains("player-2") &&
-      divEl[i + 2].classList.contains("player-2") &&
-      divEl[i + 3].classList.contains("player-2") &&
-      divEl[i] != [5, 11, 17, 23, 29, 35, 41]
-    ) {
-      alert("Player two wins!");
+function buildBoard() {
+    for (let i = 0; i < 20; i++) {
+        let space = document.createElement('div')
+        space.id = i
+        space.setAttribute('class', 'open')
+        gameBoard.appendChild(space)
     }
 }
 
-function render() {
-  for (let i = 0; i < divEl.length; i++) {
-    divEl[i].onclick = () => {
-      if (currentPlayer == 1 && divEl[i].classList.contains("open")) {
-        divEl[i].style.backgroundColor = "red";
-        divEl[i].classList.add("player-1");
-        divEl[i].classList.remove("open");
-        currentPlayer = 2;
-      } else if (currentPlayer == 2 && divEl[i].classList.contains("open")) {
-        divEl[i].style.backgroundColor = "blue";
-        divEl[i].classList.add("player-2");
-        divEl[i].classList.remove("open");
-        currentPlayer = 1;
-        check();
-        // document.querySelector(
-        //   ".players-turn"
-        // ).innerHTML = ` It's ${player2}'s turn! `;
-      } else alert("move not allowed");
+buildBoard();
 
-      // document.querySelector(
-      //   ".players-turn"
-      // ).innerHTML = ` It's ${player1}'s turn! `;
-    };
-  }
+function render(evt) {
+    let link = evt.target
+    if (link.classList.contains('open')) {
+        if (currentPlayer === 1) {
+            link.classList.remove('open')
+            link.classList.add('taken')
+            link.style.backgroundColor = "red"
+            currentPlayer = 2
+            board[link.id] = "red"
+        } else if (currentPlayer === 2) {
+            link.classList.remove('open')
+            link.classList.add('taken')
+            link.style.backgroundColor = "blue"
+            currentPlayer = 1
+            board[link.id] = "blue"
+        } check();
+    } else if (link.classList.contains('taken')) {
+        alert('Move not allowed!')
+    }   console.log(link.id) 
 }
-render()
+
+function check(){
+  
+  //horizontal hard-coded wins for red player one
+if (board[16] == "red" && board[17] == "red" && board[18] == "red" && board[19] == "red"){
+  alert("Player one wins") 
+} else if (board[12] == "red" && board[13] == "red" && board[14] == "red" && board[15] == "red"){
+  alert("Player one wins")
+} else if (board[8] == "red" && board[9] == "red" && board[10] == "red" && board[11] == "red"){
+  alert("Player one wins")
+} else if (board[4] == "red" && board[5] == "red" && board[6] == "red" && board[7] == "red"){
+  alert("Player one wins")
+} else if (board[0] == "red" && board[1] == "red" && board[2] == "red" && board[3] == "red"){
+  alert("Player one wins")
+
+//horizontal hard-coded wins for blue player two
+} else if (board[16] == "blue" && board[17] == "blue" && board[18] == "blue" && board[19] == "blue"){
+  alert("Player two wins")
+} else if (board[12] == "blue" && board[13] == "blue" && board[14] == "blue" && board[15] == "blue"){
+  alert("Player two wins")
+} else if (board[8] == "blue" && board[9] == "blue" && board[10] == "blue" && board[11] == "blue"){
+  alert("Player two wins")
+} else if (board[4] == "blue" && board[5] == "blue" && board[6] == "blue" && board[7] == "blue"){
+  alert("Player two wins")
+} else if (board[0] == "blue" && board[1] == "blue" && board[2] == "blue" && board[3] == "blue"){
+  alert("Player two wins")
+
+//vertical hard-coded wins for red player one 
+} else if (board[16] == "red" && board[12] == "red" && board[8] == "red" && board[4] == "red") {
+  alert("Player one wins")
+} else if (board[12] == "red" && board[8] == "red" && board[4] == "red" && board[0] == "red") {
+  alert("Player one wins")
+} else if (board[17] == "red" && board[13] == "red" && board[9] == "red" && board[5] == "red") {
+  alert("Player one wins")
+} else if (board[13] == "red" && board[9] == "red" && board[5] == "red" && board[1] == "red") {
+  alert("Player one wins")
+} else if (board[18] == "red" && board[14] == "red" && board[10] == "red" && board[6] == "red") {
+  alert("Player one wins")
+} else if (board[14] == "red" && board[10] == "red" && board[6] == "red" && board[2] == "red") {
+  alert("Player one wins")
+} else if (board[19] == "red" && board[15] == "red" && board[11] == "red" && board[7] == "red") {
+  alert("Player one wins")
+} else if (board[15] == "red" && board[11] == "red" && board[7] == "red" && board[3] == "red") {
+  alert("Player one wins")
+
+//vertical hard-coded wins for blue player two
+} else if (board[16] == "blue" && board[12] == "blue" && board[8] == "blue" && board[4] == "blue") {
+  alert("Player two wins")
+} else if (board[12] == "blue" && board[8] == "blue" && board[4] == "blue" && board[0] == "blue") {
+  alert("Player two wins")
+} else if (board[17] == "blue" && board[13] == "blue" && board[9] == "blue" && board[5] == "blue") {
+  alert("Player two wins")
+} else if (board[13] == "blue" && board[9] == "blue" && board[5] == "blue" && board[1] == "blue") {
+  alert("Player two wins")
+} else if (board[18] == "blue" && board[14] == "blue" && board[10] == "blue" && board[6] == "blue") {
+  alert("Player two wins")
+} else if (board[14] == "blue" && board[10] == "blue" && board[6] == "blue" && board[2] == "blue") {
+  alert("Player two wins")
+} else if (board[19] == "blue" && board[15] == "blue" && board[11] == "blue" && board[7] == "blue") {
+  alert("Player two wins")
+} else if (board[15] == "blue" && board[11] == "blue" && board[7] == "blue" && board[3] == "blue") {
+  alert("Player two wins")
+ 
+// Diagonal wins for red player one
+}else if (board[12] == "red" && board[9] == "red" && board[6] == "red" && board[3] == "red") {
+  alert("Player one wins")
+}else if (board[16] == "red" && board[13] == "red" && board[10] == "red" && board[7] == "red") {
+  alert("Player one wins")
+}else if (board[19] == "red" && board[14] == "red" && board[9] == "red" && board[4] == "red") {
+  alert("Player one wins")
+}else if (board[15] == "red" && board[10] == "red" && board[5] == "red" && board[0] == "red") {
+  alert("Player one wins")
+
+// Diagonal wins for blue player two  
+} else if (board[12] == "blue" && board[9] == "blue" && board[6] == "blue" && board[3] == "blue") {
+  alert("Player two wins")
+}else if (board[16] == "blue" && board[13] == "blue" && board[10] == "blue" && board[7] == "blue") {
+  alert("Player two wins")
+}else if (board[19] == "blue" && board[14] == "blue" && board[9] == "blue" && board[4] == "blue") {
+  alert("Player two wins")
+}else if (board[15] == "blue" && board[10] == "blue" && board[5] == "blue" && board[0] == "blue") {
+  alert("Player two wins")
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const player1 = window.prompt("Enter name for player 1: ");
+// // const player2 = window.prompt("Enter name for player 2: ");
+
+// // document.querySelector(".players-names").innerHTML = `${player1} vs ${player2}`;
+
+// // function createDivs(){
+// // for ( i = 0; i< 42; i++){
+// //    let newDiv = document.createElement("div")
+// //    divEl.appendChild(newDiv);
+// // }
+// // }
+// // createDivs();
+
+// // const divArray = [];
+// // for (let i = 0; i < divEl.length; i++){
+// //     divArray.push(divEl[i])
+// // }
+
+
+
+// let currentPlayer = 1;
+
+// // const divEl = document.querySelectorAll(".grid-container");
+
+
+// function buildBoard() {
+//   let board = []
+//   for (let i = 0; i < 42; i++) {
+//       let square = document.createElement('div')
+//       square.id = i
+//       square.setAttribute('class', 'square')
+//       document.body.appendChild(square)
+//   }
+// }
+
+// buildBoard()
+
+// function check() {
+//   for (let i = 0; i < divEl.length; i++)
+//     //Check for a horizontal win for player one
+//     if (
+//       divEl[i].classList.contains("player-1") &&
+//       divEl[i + 1].classList.contains("player-1") &&
+//       divEl[i + 2].classList.contains("player-1") &&
+//       divEl[i + 3].classList.contains("player-1") &&
+//       divEl[i] != [5, 11, 17, 23, 29, 35, 41]
+//     ) {
+//       alert("Player one wins!");
+      
+//     } else if (
+//       divEl[i].classList.contains("player-2") &&
+//       divEl[i + 1].classList.contains("player-2") &&
+//       divEl[i + 2].classList.contains("player-2") &&
+//       divEl[i + 3].classList.contains("player-2") &&
+//       divEl[i] != [5, 11, 17, 23, 29, 35, 41]
+//     ) {
+//       alert("Player two wins!");
+      
+//     }
+// }
+
+
+// // render()
